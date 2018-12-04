2778bfa4df81a5a8c49f7d865ac475a136ef18dd
MaheshNawle <44395318+MaheshNawle@users.noreply.github.com>
2018-12-04 13:53:00 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,15 @@
 #FACTORIAL
 #MAHESH NAWALE
 #043
-x=int(input())
+x=int(input("ENTER THE NUMBER TO IT'S FACTORIAL="))
+y=x
+total=1
 if x<0:
-  print("no factorial exist")
+   print("FACTORIAL DOES NO EXIST")
 elif x==0 or x==1:
-  print("factorial is 1")
+   print("FACTORIAL OF"+STR(Y)+"IS 1"
 else:
-  print(
-
+   while x>0:
+      total =x*total
+      x-=1
+   print("FACTORIAL OF"+str(y)+"IS"+str(total))
